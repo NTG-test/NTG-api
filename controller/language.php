9e6fdac2e3beadeb4400dfe0dd4e97d31d0106eb
nema <40147733+nexnema@users.noreply.github.com>
2021-09-24 22:31:50 +0330

    nwaLang name

--- controller/language.php
+++ controller/language.php
@@ -3,12 +3,16 @@
         if ($_GET['id']=='') done(400, 'langCodeIsNotSet');
 
         $allowedLang = array('enUS', 'ar', 'fa');
-        if (!in_array($_GET['id'], $allowedLang)) done(404,'langCodeNotFound');
+        if (!in_array($_GET['id'], $allowedLang)) done(404, 'langCodeNotFound');
 
-        $result = mysqli_query($GLOBALS['db'], "SELECT * FROM ntgLanguage ");
+        $result = mysqli_query($GLOBALS['db'], "SELECT * FROM nwaLanguage");
         $i=1;
-        while ($quranSurah = mysqli_fetch_array($result)) {
-            $response -> $i = (object) ['selector'=>$quranSurah['selector'], 'attribute'=>$quranSurah['attribute'], 'trans'=>$quranSurah[$_GET['id']]];
+        while ($nwaLanguage = mysqli_fetch_array($result)) {
+            $response -> $i = (object) [
+                'selector'=>$nwaLanguage['selector'],
+                'attribute'=>$nwaLanguage['attribute'],
+                'trans'=>$nwaLanguage[$_GET['id']]
+            ];
             $i++;
         }
         done(200, json_encode($response, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
