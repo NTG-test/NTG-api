496922aeddbe78fefc2fe4bc1e59ab5fcfd17a52
nema <40147733+nexnema@users.noreply.github.com>
2021-11-19 21:33:26 +0330

    0.1.1

--- controllers/language.php
+++ controllers/language.php
@@ -6,17 +6,18 @@
 		$allowedLang = array('enUS', 'ar', 'fa');
 		if (!in_array($GLOBALS['nwaApi']->id, $allowedLang)) $GLOBALS['nwaApi']->done(404, 'langCodeNotFound');
 		
-		$GLOBALS['nwaApi']->done(200, 'lang');
+		// $result = mysqli_query($GLOBALS['db'], "SELECT * FROM nwaLanguage");
+		// $i=1;
+		// while ($nwaLanguage = mysqli_fetch_array($result)) {
+		// 	$response->$i = (object) [
+		// 		'selector'=>$nwaLanguage['selector'],
+		// 		'attribute'=>$nwaLanguage['attribute'],
+		// 		'trans'=>$nwaLanguage[$_GET['id']]
+		// 	];
+		// 	$i++;
+		// }
 
-		$result = mysqli_query($GLOBALS['db'], "SELECT * FROM nwaLanguage");
-		$i=1;
-		while ($nwaLanguage = mysqli_fetch_array($result)) {
-			$response -> $i = (object) [
-				'selector'=>$nwaLanguage['selector'],
-				'attribute'=>$nwaLanguage['attribute'],
-				'trans'=>$nwaLanguage[$_GET['id']]
-			];
-			$i++;
-		}
-		done(200, json_encode($response, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+		$GLOBALS['nwaApi']->massage('Test massage from language.');
+		$GLOBALS['nwaApi']->responseData = $response;
+		$GLOBALS['nwaApi']->done(200, 'CMC Data');
 	}
