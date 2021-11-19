66c3efac66b14c425342b3e5bf11aef0098df351
nema <40147733+nexnema@users.noreply.github.com>
2021-11-19 21:25:07 +0330

    0.1.1

--- controllers/language.php
+++ controllers/language.php
@@ -1,19 +1,21 @@
 <?php
-    function GET() {
-        if ($_GET['id']=='') done(400, 'langCodeIsNotSet');
+	function GET() {
+		$GLOBALS['nwaApi']->done(200, 'lang');
 
-        $allowedLang = array('enUS', 'ar', 'fa');
-        if (!in_array($_GET['id'], $allowedLang)) done(404, 'langCodeNotFound');
+		if ($_GET['id']=='') done(400, 'langCodeIsNotSet');
 
-        $result = mysqli_query($GLOBALS['db'], "SELECT * FROM nwaLanguage");
-        $i=1;
-        while ($nwaLanguage = mysqli_fetch_array($result)) {
-            $response -> $i = (object) [
-                'selector'=>$nwaLanguage['selector'],
-                'attribute'=>$nwaLanguage['attribute'],
-                'trans'=>$nwaLanguage[$_GET['id']]
-            ];
-            $i++;
-        }
-        done(200, json_encode($response, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
-    }
+		$allowedLang = array('enUS', 'ar', 'fa');
+		if (!in_array($_GET['id'], $allowedLang)) done(404, 'langCodeNotFound');
+
+		$result = mysqli_query($GLOBALS['db'], "SELECT * FROM nwaLanguage");
+		$i=1;
+		while ($nwaLanguage = mysqli_fetch_array($result)) {
+			$response -> $i = (object) [
+				'selector'=>$nwaLanguage['selector'],
+				'attribute'=>$nwaLanguage['attribute'],
+				'trans'=>$nwaLanguage[$_GET['id']]
+			];
+			$i++;
+		}
+		done(200, json_encode($response, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+	}
