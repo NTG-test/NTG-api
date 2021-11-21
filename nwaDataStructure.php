a1842ffa9755aaa0e20d216db8248643cfcbf1ae
nema <40147733+nexnema@users.noreply.github.com>
2021-11-21 20:02:56 +0330

    0.1.1

--- nwaDataStructure.php
+++ nwaDataStructure.php
@@ -1,9 +1,8 @@
 <?php
 
 function nwaCreateNwaDatabaseTables() {
-	if (!$GLOBALS['db']->query('DESCRIBE nwaRequest')) {
 		$GLOBALS['db']->query(
-			"CREATE TABLE nwaRequest (
+			"CREATE TABLE IF NOT EXISTS nwaRequest (
 			id INT(16) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 			time INT(4),
 			userId INT(16),
@@ -19,11 +18,9 @@ function nwaCreateNwaDatabaseTables() {
 			response LONGTEXT
 			) CHARSET=utf8 COLLATE utf8_unicode_ci"
 		);
-		$GLOBALS['nwaApi']->massage('Table nwaRequest created.');
-	}
-	if (!$GLOBALS['db']->query('DESCRIBE nwaLanguage')) {
+
 		$GLOBALS['db']->query(
-			"CREATE TABLE nwaLanguage (
+			"CREATE TABLE IF NOT EXISTS nwaLanguage (
 			id INT(16) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 			selector VARCHAR(256),
 			attribute VARCHAR(32),
@@ -31,6 +28,4 @@ function nwaCreateNwaDatabaseTables() {
 			fa VARCHAR(256)
 			) CHARSET=utf8 COLLATE utf8_unicode_ci"
 		);
-		$GLOBALS['nwaApi']->massage('Table nwaLanguage created.');
-	}
 }
