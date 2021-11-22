47b848a1de7f4b0590b6c75086af4691a3736610
nema <40147733+nexnema@users.noreply.github.com>
2021-11-22 09:25:43 +0330

    0.1.1

--- controllers/nwaHelp.php
+++ controllers/nwaHelp.php
@@ -1,21 +1,21 @@
 <?php
 
-class nwaAcceptedRequestHelp() {
-	public $controller;
-	public $methods = array();
+// class nwaAcceptedRequestHelp() {
+// 	public $controller;
+// 	public $methods = array();
 
-	public getHelpDataForController($file) {
-		$fileName = pathinfo($file)['filename'];
-		preg_match_all('/function (\w+)/', file_get_contents($file), $functionList);
-		$this->controller = $fileName;
-		foreach ($functionList[1] as $functionName)
-		if (in_array($functionName, array('GET', 'PUT', 'POST', 'DELETE', 'OPTIONS')))
-			array_push($this->methods, $functionName);
-	}
-}
+// 	public getHelpDataForController($file) {
+// 		$fileName = pathinfo($file)['filename'];
+// 		preg_match_all('/function (\w+)/', file_get_contents($file), $functionList);
+// 		$this->controller = $fileName;
+// 		foreach ($functionList[1] as $functionName)
+// 		if (in_array($functionName, array('GET', 'PUT', 'POST', 'DELETE', 'OPTIONS')))
+// 			array_push($this->methods, $functionName);
+// 	}
+// }
 
 
-//API supported controllers & methods Help List
+// API supported controllers & methods Help List
 function GET() {
 	// foreach(glob('controllers/*.php') as $file) {
 	// 	$nwaAcceptedRequestHelp = new nwaAcceptedRequestHelp();
