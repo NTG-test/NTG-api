7d3df4fe78831f0b30c768a916c6ec9ad8f080c7
nema <40147733+nexnema@users.noreply.github.com>
2021-11-22 12:41:38 +0330

    0.1.1

--- app/dataStructure.php
+++ app/dataStructure.php
@@ -3,17 +3,17 @@
 namespace app;
 
 function createDatabaseTables() {
-	if (!$GLOBALS['db'] -> query('DESCRIBE appCoins')) {
-		$GLOBALS['db'] -> query(
-			"CREATE TABLE IF NOT EXISTS appCoins (
-			id INT(16) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-			cmcId INT(16),
-			symbol VARCHAR(256),
-			name VARCHAR(1024),
-			platformCmcId INT(16),
-			platformSymbol VARCHAR(128),
-			platformContract VARCHAR(1024)
-			) CHARSET=utf8 COLLATE utf8_unicode_ci"
-		);
-	}
+	// if (!$GLOBALS['db'] -> query('DESCRIBE appCoins')) {
+	// 	$GLOBALS['db'] -> query(
+	// 		"CREATE TABLE IF NOT EXISTS appCoins (
+	// 		id INT(16) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	// 		cmcId INT(16),
+	// 		symbol VARCHAR(256),
+	// 		name VARCHAR(1024),
+	// 		platformCmcId INT(16),
+	// 		platformSymbol VARCHAR(128),
+	// 		platformContract VARCHAR(1024)
+	// 		) CHARSET=utf8 COLLATE utf8_unicode_ci"
+	// 	);
+	// }
 }
