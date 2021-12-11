6720e8691b5bd8455e0f760cb8cf6dd6ec7e342c
nema <40147733+nexnema@users.noreply.github.com>
2021-12-11 11:59:30 +0330

    0.1.7

--- nwaApi.php
+++ nwaApi.php
@@ -2,7 +2,7 @@
 
 namespace nwa;
 
-class api {
+class request {
 	public $adminMailAddress;
 	public $ip;
 	public $token;
@@ -13,10 +13,6 @@ class api {
 	public $userId;
 	public $businessId;
 
-	private $httpResponseCode;
-	public $data = array();
-	private $response;
-
 	function __construct() {
 		if (isset($_ENV['ADMIN_EMAIL_ADDRESS']))	$this->adminMailAddress = $_ENV['ADMIN_EMAIL_ADDRESS'];
 		if (isset($_SERVER['REMOTE_ADDR']))			$this->ip = $_SERVER['REMOTE_ADDR'];
@@ -26,6 +22,50 @@ class api {
 		if (isset($_GET['action']))					$this->action = $_GET['action'];
 		if (isset($_GET['id']))						$this->id = $_GET['id'];
 	}
+
+	// private function logRequestAndResponseToDb() {
+	// 	$GLOBALS['db'] -> query(
+	// 		"INSERT INTO nwaRequest (
+	// 			httpResponseCode,
+	// 			response,
+	// 			time,
+	// 			controller,
+	// 			method,
+	// 			action,
+	// 			requestedId,
+	// 			remoteAddr,
+	// 			token
+	// 		) VALUES (
+	// 			'".$this->httpResponseCode."',
+	// 			'".$this->response."',
+	// 			'".time()."',
+	// 			'".$this->controller."',
+	// 			'".$this->method."',
+	// 			'".$this->action."',
+	// 			'".$this->id."',
+	// 			'".$this->ip."',
+	// 			'".$this->token."'
+	// 		)
+	// 	");
+	// }
+	// private function emailErrorToAdmin() {
+	// 	if ($this->httpResponseCode >= 300) {
+	// 		mail(
+	// 			$this->adminMailAddress,
+	// 			'NWA API Log '.$this->httpResponseCode,
+	// 			'Ip: '.$this->ip.'<br>'.
+	// 			'Response: '.$this->response.'<br>'.
+	// 			'Token: '.$this->token.'<br>'
+	// 			// 'MIME-Version: 1.0\r\nContent-type:text/html;charset=UTF-8\r\nFrom: noreply@cleveraj.com'
+	// 		);
+	// 	}
+	// }
+}
+
+class response {
+	private $httpResponseCode;
+	public $data = array();
+	private $response;
 
 	// Create response, Exit request
 	public function done($httpResponseCode, $responseFinalMassage = null) {
@@ -46,43 +86,5 @@ class api {
 
 		http_response_code($this->httpResponseCode);
 		exit($this->response);
-	}
-	
-	private function logRequestAndResponseToDb() {
-		$GLOBALS['db'] -> query(
-			"INSERT INTO nwaRequest (
-				httpResponseCode,
-				response,
-				time,
-				controller,
-				method,
-				action,
-				requestedId,
-				remoteAddr,
-				token
-			) VALUES (
-				'".$this->httpResponseCode."',
-				'".$this->response."',
-				'".time()."',
-				'".$this->controller."',
-				'".$this->method."',
-				'".$this->action."',
-				'".$this->id."',
-				'".$this->ip."',
-				'".$this->token."'
-			)
-		");
-	}
-	private function emailErrorToAdmin() {
-		if ($this->httpResponseCode >= 300) {
-			mail(
-				$this->adminMailAddress,
-				'NWA API Log '.$this->httpResponseCode,
-				'Ip: '.$this->ip.'<br>'.
-				'Response: '.$this->response.'<br>'.
-				'Token: '.$this->token.'<br>'
-				// 'MIME-Version: 1.0\r\nContent-type:text/html;charset=UTF-8\r\nFrom: noreply@cleveraj.com'
-			);
-		}
 	}
 }
